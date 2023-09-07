32738c8ab5abcaa3ddab9837f53640c67e01a7c2
PauGOC <141955969+PauGOC@users.noreply.github.com>
2023-09-06 22:39:17 -0600

    Add files via upload

---
+++ Programa que imprime 10 numeros mas 177523.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+	
+    	int a=0;
+
+    	printf("Introduce un número: ");
+    	scanf("%d", &a);
+    	
+    	printf("Los siguientes 10 números son: \n");
+    	printf("%d\n",a+1);
+    	printf("%d\n",a+2);
+    	printf("%d\n",a+3);
+    	printf("%d\n",a+4);
+    	printf("%d\n",a+5);
+    	printf("%d\n",a+6);
+    	printf("%d\n",a+7);
+    	printf("%d\n",a+8);
+    	printf("%d\n",a+9);
+    	printf("%d\n",a+10);
+	return 0;		
+	
+}
